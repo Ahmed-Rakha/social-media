7f46b075593f49ede72444ec35ba13e733a3106a
Ahmed-Rakha <rakhaahmed1@gmail.com>
2026-02-19 19:11:59 +0200

    [Feature] feature/add type checking of notification services

--- src/services/notifications/mark_notification_as_read.js
+++ src/services/notifications/mark_notification_as_read.js
@@ -1,5 +1,8 @@
 import { $API } from "../../api/axios";
 
+/**
+ * @param {string} notificationId - The notificationId is required
+ */
 export const markNotificationAsRead = async (notificationId) => {
   const ROUTE = `notifications/${notificationId}/read`;
 
