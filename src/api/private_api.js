92f33fe8d924a08d5840d654a92dc71e0914abe2
Ahmed-Rakha <rakhaahmed1@gmail.com>
2026-02-19 12:41:38 +0200

    [Feature] feature/ add privateApi.interceptors.response

--- src/api/private_api.js
+++ src/api/private_api.js
@@ -20,3 +20,14 @@ privateApi.interceptors.request.use(
     return Promise.reject(error);
   },
 );
+
+privateApi.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.removeItem("social-app-token");
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  },
+);
